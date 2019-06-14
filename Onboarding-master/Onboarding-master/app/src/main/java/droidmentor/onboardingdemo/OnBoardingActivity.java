f25ce1eda46e1edd769d88e3bc4fccf04ab21f9e
Esdras Silva <esdrasilva@gmail.com>
2019-06-14 12:15:09 -0300

    Ajustes Login

--- Onboarding-master/Onboarding-master/app/src/main/java/droidmentor/onboardingdemo/OnBoardingActivity.java
+++ Onboarding-master/Onboarding-master/app/src/main/java/droidmentor/onboardingdemo/OnBoardingActivity.java
@@ -2,6 +2,7 @@ package droidmentor.onboardingdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +14,19 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
 
-
+    private static final int RC_SIGN_IN = 100;
     private LinearLayout pager_indicator;
     private int dotsCount;
     private ImageView[] dots;
@@ -91,8 +99,21 @@ public class OnBoardingActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    startActivity(new Intent(getApplicationContext(), Login.class));
-                    finish();
+//                    startActivity(new Intent(getApplicationContext(), Login.class));
+//                    finish();
+
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build());
+
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .setLogo(R.drawable.aula)      // Set logo drawable
+//                                    .setTheme(R.style.MySuperAppTheme)
+                                    .build(),
+                            RC_SIGN_IN);
                 }
 
             });
@@ -102,6 +123,27 @@ public class OnBoardingActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                    finish();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+
+    }
 
     // Load data into the viewpager
 
